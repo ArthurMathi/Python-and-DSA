e84309ddc3a621c4c3b9a0c4004805a83cd9d151
Arthur Mathi <mathibharathi426@gmail.com>
2026-02-17 09:58:34 +0530

    Create Demo.py

---
+++ Demo.py
@@ -0,0 +1,99 @@
+# currentAge=20
+# MaximumAge=80
+# AmountPerDay=100
+# totalAmount= (MaximumAge-currentAge)*365*AmountPerDay
+# print("You will need $",totalAmount," to last you until the ripe old age of ",MaximumAge)
+
+
+# # waterconsumptionPerDay
+
+# currentAge=20
+# MaximumAge=80
+# waterconsumptionPerDay=8
+# yearsRemaining=MaximumAge-currentAge
+# daysRemaining=yearsRemaining*365
+# totalWaterConsumption=daysRemaining*waterconsumptionPerDay
+# print("You will need ",totalWaterConsumption," liters of water to last you until the ripe old age of ",MaximumAge)
+
+# # car fuel cost
+
+# totalDistance=300
+# carMilage=15
+# fuelCostPerLiter=100
+# fuelRequired=totalDistance/carMilage
+# totalFuelCost=fuelRequired*fuelCostPerLiter
+# print("The total fuel cost for the trip is $",totalFuelCost)
+
+# # mobile data usage
+# dataUsagePerDay=10
+# daysInMonth=30
+# totalDataUsage=dataUsagePerDay*daysInMonth
+# print("You will use ",totalDataUsage," GB of data in a month.")
+
+# # Age in Days
+# currentAge=20
+# ageInDays=currentAge*365
+# print("You are ",ageInDays," days old.")
+
+# # shopping discount
+# originalPrice=1000
+# discountPercentage=20
+# percentaeInDecimal=discountPercentage/100
+# discountAmount=originalPrice*percentaeInDecimal
+# finalPrice=originalPrice-discountAmount
+# print("The final price after a ",discountPercentage,"% discount is $",finalPrice)
+
+# # Area of wall
+# length=5
+# width=6
+# area=length*width
+# print("The area of the wall is ",area," square meters.")
+
+# relate two integers
+# num1=int(input("Enter the first number: "))
+# num2=int(input("Enter the second number: "))
+# if num1>num2:
+#     print(num1," is greater than ",num2)
+# elif num1<num2:
+#     print(num1," is less than ",num2)
+# else:
+#     print(num1," is equal to ",num2)
+     
+# check alphabet vowel or consonant
+# alphabet=input("Enter an alphabet: ")
+# if alphabet ==['a','e','i','o','u','A','E','I','O','U']:
+#     print(alphabet," is a vowel.")  
+# else:    
+#     print(alphabet," is a consonant.")
+
+# Automatic grading system
+# marks=int(input("Enter your marks: "))
+# if marks==100:
+#     print("Grade: S")
+# elif marks>=90:
+#     print("Grade: A")
+# elif marks>=80:
+#     print("Grade: B")
+# elif marks>=70:
+#     print("Grade: C")
+# elif marks>=60:
+#     print("Grade: D")  
+# elif marks>=50:
+#     print("Grade: E")    
+# else:
+#     print("Grade: F") 
+
+# find profit or loss
+numberOfBananas=12
+costPricePerBanana=5.0
+sellingPricePerBanana=7.0
+totalCostPrice=numberOfBananas*costPricePerBanana
+totalSellingPrice=numberOfBananas*sellingPricePerBanana
+if totalSellingPrice>totalCostPrice:
+    profit=totalSellingPrice-totalCostPrice
+    print("You made a profit of $",profit)
+elif totalSellingPrice<totalCostPrice:
+    loss=totalCostPrice-totalSellingPrice
+    print("You incurred a loss of $",loss)
+else:
+    print("You broke even, no profit or loss.")
